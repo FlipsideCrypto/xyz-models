0f9ac89d2a85e9290dac394a3881ddd7039427b7
xiuy001 <xiuyang@flipsidecrypto.com>
2023-05-04 10:21:30 -0400

    updated

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -1,2 +1,7 @@
 {% macro create_udfs() %}
+    {{ create_udtf_get_base_table(
+        schema = "streamline"
+    ) }}
+    {{ create_udf_get_chainhead() }}
+    {{ create_udf_json_rpc() }}
 {% endmacro %}
---
+++ macros/streamline/streamline_udfs.sql
@@ -0,0 +1,18 @@
+{% macro create_udf_get_chainhead() %}
+    CREATE EXTERNAL FUNCTION IF NOT EXISTS streamline.udf_get_chainhead() returns variant api_integration = aws_evmos_api AS {% if target.name == "prod" %}
+        'https://55h4rahr50.execute-api.us-east-1.amazonaws.com/dev/get_chainhead'
+    {% else %}
+        'https://55h4rahr50.execute-api.us-east-1.amazonaws.com/dev/get_chainhead'
+    {%- endif %};
+{% endmacro %}
+
+{% macro create_udf_json_rpc() %}
+    CREATE
+    OR REPLACE EXTERNAL FUNCTION streamline.udf_json_rpc(
+        json OBJECT
+    ) returns ARRAY api_integration = aws_evmos_api AS {% if target.name == "prod" %}
+        'https://55h4rahr50.execute-api.us-east-1.amazonaws.com/dev/bulk_get_json_rpc'
+    {% else %}
+        'https://55h4rahr50.execute-api.us-east-1.amazonaws.com/dev/bulk_get_json_rpc'
+    {%- endif %};
+{% endmacro %}
