913253effd6eade8e9cc7a7421508e9ef01dcd28
xiuy001 <xiuyang@flipsidecrypto.com>
2023-06-28 14:29:01 -0400

    update the streamline config for prod

--- macros/streamline/api_integrations.sql
+++ macros/streamline/api_integrations.sql
@@ -1,14 +1,14 @@
 {% macro create_aws_aurora_api() %}
     {% if target.name == "prod" %}
         {% set sql %}
-        CREATE api integration IF NOT EXISTS aws_bitcoin_api api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::490041342817:role/bitcoin-api-dev-rolesnowflakeudfsAF733095-15K9O939DPMBA' api_allowed_prefixes = (
-            'https://f81vesdos6.execute-api.us-east-1.amazonaws.com/dev/'
+        CREATE api integration IF NOT EXISTS aws_bitcoin_api api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::490041342817:role/bitcoin-api-prod-rolesnowflakeudfsAF733095-YHUYTRLX42SE' api_allowed_prefixes = (
+            'https://9iyki2rp1b.execute-api.us-east-1.amazonaws.com/prod/'
         ) enabled = TRUE;
         {% endset %}
         {% do run_query(sql) %}
     {% else %}
         {% set sql %}
-        CREATE api integration IF NOT EXISTS aws_bitcoin_api api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::490041342817:role/bitcoin-api-dev-rolesnowflakeudfsAF733095-15K9O939DPMBA' api_allowed_prefixes = (
+        CREATE api integration IF NOT EXISTS aws_bitcoin_dev_api api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::490041342817:role/bitcoin-api-dev-rolesnowflakeudfsAF733095-15K9O939DPMBA' api_allowed_prefixes = (
             'https://f81vesdos6.execute-api.us-east-1.amazonaws.com/dev/'
         ) enabled = TRUE;
         {% endset %}
--- macros/streamline/streamline_udfs.sql
+++ macros/streamline/streamline_udfs.sql
@@ -1,17 +1,23 @@
 {% macro create_udf_get_chainhead() %}
-    CREATE EXTERNAL FUNCTION IF NOT EXISTS streamline.udf_get_chainhead() returns variant api_integration = aws_bitcoin_api AS {% if target.name == "prod" %}
-        'https://f81vesdos6.execute-api.us-east-1.amazonaws.com/dev/get_chainhead'
+    {% if target.name == "prod" %}
+        CREATE EXTERNAL FUNCTION IF NOT EXISTS streamline.udf_get_chainhead() returns variant api_integration = aws_bitcoin_api AS 
+            'https://9iyki2rp1b.execute-api.us-east-1.amazonaws.com/prod/get_chainhead'
     {% else %}
-        'https://f81vesdos6.execute-api.us-east-1.amazonaws.com/dev/get_chainhead'
+        CREATE EXTERNAL FUNCTION IF NOT EXISTS streamline.udf_get_chainhead() returns variant api_integration = aws_bitcoin_dev_api AS 
+            'https://f81vesdos6.execute-api.us-east-1.amazonaws.com/dev/get_chainhead'  
     {%- endif %};
 {% endmacro %}
 
 {% macro create_udf_json_rpc() %}
-    CREATE EXTERNAL FUNCTION IF NOT EXISTS streamline.udf_json_rpc(
-        json OBJECT
-    ) returns ARRAY api_integration = aws_bitcoin_api AS {% if target.name == "prod" %}
-        'https://f81vesdos6.execute-api.us-east-1.amazonaws.com/dev/bulk_get_json_rpc'
+    {% if target.name == "prod" %}
+        CREATE EXTERNAL FUNCTION IF NOT EXISTS streamline.udf_json_rpc(
+            json OBJECT
+        ) returns ARRAY api_integration = aws_bitcoin_api AS 
+            'https://9iyki2rp1b.execute-api.us-east-1.amazonaws.com/prod/bulk_get_json_rpc'
     {% else %}
-        'https://f81vesdos6.execute-api.us-east-1.amazonaws.com/dev/bulk_get_json_rpc'
+        CREATE EXTERNAL FUNCTION IF NOT EXISTS streamline.udf_json_rpc(
+            json OBJECT
+        ) returns ARRAY api_integration = aws_bitcoin_dev_api AS 
+            'https://f81vesdos6.execute-api.us-east-1.amazonaws.com/dev/bulk_get_json_rpc'
     {%- endif %};
 {% endmacro %}
