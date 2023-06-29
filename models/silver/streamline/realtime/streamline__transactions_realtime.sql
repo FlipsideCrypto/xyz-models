c8f89654dd48570512a0e96b64295213994cae96
xiuy001 <xiuyang@flipsidecrypto.com>
2023-06-29 03:14:22 -0400

    update the params

--- models/silver/streamline/realtime/streamline__transactions_realtime.sql
+++ models/silver/streamline/realtime/streamline__transactions_realtime.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_json_rpc(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'transactions', 'exploded_key','[\"result\", \"tx\"]', 'producer_batch_size',100, 'producer_limit_size', 10000, 'worker_batch_size',10))",
+        func = "{{this.schema}}.udf_json_rpc(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'transactions', 'exploded_key','[\"result\", \"tx\"]', 'producer_batch_size',100, 'producer_limit_size', 1000000, 'worker_batch_size',10))",
         target = "{{this.schema}}.{{this.identifier}}"
     )
 ) }}
