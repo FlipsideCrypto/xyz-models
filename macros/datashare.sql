9f1120a67ea9b3aafbff79638973a5f729425e06
Julius Remigio <14811322+juls858@users.noreply.github.com>
2023-07-05 17:32:03 -0700

    - upper keywords

--- macros/datashare.sql
+++ macros/datashare.sql
@@ -1,45 +1,45 @@
 {% macro create_sp_grant_share_permissions_string_timestamp() %}
-create or replace procedure datashare.sp_grant_share_permissions(db string, updated_since TIMESTAMP_NTZ)
-returns table (sql string)
+CREATE OR REPLACE PROCEDURE datashare.sp_grant_share_permissions(db STRING, updated_since TIMESTAMP_NTZ)
+RETURNS TABLE (SQL STRING)
 LANGUAGE SQL
-execute as caller
-as
+EXECUTE AS CALLER
+AS
 $$
 DECLARE
-  share_name varchar default :db || '_FLIPSIDE_AF';
-  ddl_views varchar default :db || '._datashare._create_gold';
-  target_schema varchar default :db || '.information_schema.schemata';
-  all_grants varchar default '';
+  share_name VARCHAR DEFAULT :db || '_FLIPSIDE_AF';
+  ddl_views VARCHAR DEFAULT :db || '._datashare._create_gold';
+  target_schema VARCHAR DEFAULT :db || '.information_schema.schemata';
+  all_grants VARCHAR DEFAULT '';
 BEGIN
-    with target_schema as (
-        select
-            array_agg(schema_name) as schema_name
-        from identifier(:target_schema)
+    WITH target_schema AS (
+        SELECT
+            array_agg(schema_name) AS schema_name
+        FROM identifier(:target_schema)
     ),
-    schema_grants as (
-        select
-            regexp_substr_all(ddl,'(__SOURCE__).([\\w]+)', 1, 1,'i', 2) as extracted
-            , array_distinct(split(upper(array_to_string(extracted, ';')), ';')) as distinct_schemata
+    schema_grants AS (
+        SELECT
+            regexp_substr_all(ddl,'(__SOURCE__).([\\w]+)', 1, 1,'i', 2) AS extracted
+            , array_distinct(split(upper(array_to_string(extracted, ';')), ';')) AS distinct_schemata
             , array_intersection(
                 array_append(distinct_schemata, '_DATASHARE'),
-                (select schema_name from target_schema)
-            ) as valid_schemata,
+                (SELECT schema_name FROM target_schema)
+            ) AS valid_schemata,
              regexp_replace(
                 array_to_string(valid_schemata, ';'),
                 '([\\w]+)',
                 '\nGRANT USAGE ON SCHEMA {DB}.\\1 TO SHARE {SHARE};' ||
                 '\nGRANT SELECT ON ALL TABLES IN SCHEMA {DB}.\\1 TO SHARE {SHARE}'
-            ) || ';\n' as ddl
-        from
+            ) || ';\n' AS ddl
+        FROM
             identifier(:ddl_views)
-        where
+        WHERE
             ddl_created_at >= :updated_since
-        order by
-            ddl_created_at desc
-        limit
+        ORDER BY
+            ddl_created_at DESC
+        LIMIT
             1
     )
-    select concat_ws(' ',
+    SELECT concat_ws(' ',
         'CREATE SHARE IF NOT EXISTS', :share_name, ';\n',
         'GRANT USAGE ON DATABASE',:db, 'TO SHARE', :share_name, ';\n',
         replace(
@@ -49,49 +49,51 @@ BEGIN
                 :db),
             '{SHARE}',
             :share_name)
-        ) as permissions
-     into :all_grants
-    from schema_grants s;
+        ) AS permissions
+     INTO :all_grants
+    FROM schema_grants s;
 
-    IF (all_grants is not null) then
+    IF (all_grants IS NOT NULL) THEN
       EXECUTE IMMEDIATE 'BEGIN\n' || :all_grants || 'END\n';
-  end if;
-  let rs resultset := (select value
-      from table(split_to_table(:all_grants,'\n'))
-      where value not in  ('', ' '));
-  RETURN table(rs);
+    END IF;
+    LET rs RESULTSET := (
+        SELECT value
+        FROM TABLE(split_to_table(:all_grants,'\n'))
+        WHERE value NOT IN  ('', ' ')
+      );
+  RETURN TABLE(rs);
 END;
 $$
 ;
 {% endmacro %}
 
 {% macro create_sp_grant_share_permissions_timestamp() %}
-CREATE OR REPLACE procedure datashare.sp_grant_share_permissions(updated_since TIMESTAMP_NTZ)
-returns table (TABLE_CATALOG string)
+CREATE OR REPLACE PROCEDURE datashare.sp_grant_share_permissions(updated_since TIMESTAMP_NTZ)
+RETURNS TABLE (TABLE_CATALOG STRING)
 LANGUAGE SQL
-as
+AS
 $$
 DECLARE
-  cur CURSOR FOR  select
+    cur CURSOR FOR SELECT
         table_catalog
-    from snowflake.account_usage.tables
-    where table_name = '_CREATE_GOLD'
+    FROM snowflake.account_usage.tables
+    WHERE table_name = '_CREATE_GOLD'
     and table_schema = '_DATASHARE'
     AND TABLE_CATALOG not like '%_DEV';
 BEGIN
     create or replace temporary table results as
-    select ''::string as db
-    from dual
-    limit 0;
+    SELECT ''::string AS db
+    FROM dual
+    LIMIT 0;
     FOR cur_row IN cur DO
-        let db varchar:= cur_row.table_catalog;
-        call datashare.sp_grant_share_permissions(:db, :updated_since);
-        let cnt varchar := (select count(*) from table(result_scan(last_query_id())));
-        if (cnt > 0) then
-            insert into results (db) values (:db);
-        end if;
+        LET db VARCHAR := cur_row.table_catalog;
+        CALL datashare.sp_grant_share_permissions(:db, :updated_since);
+        LET cnt VARCHAR := (SELECT COUNT(*) FROM TABLE(result_scan(last_query_id())));
+        IF (cnt > 0) THEN
+            INSERT INTO RESULTS (db) VALUES (:db);
+        END IF;
     END FOR;
-    let rs resultset := (select * from results order by db);
+    LET rs RESULTSET := (SELECT * FROM results ORDER BY db);
     RETURN TABLE(rs);
 END;
 $$
@@ -99,34 +101,34 @@ $$
 {% endmacro %}
 
 {% macro create_sp_grant_share_permissions() %}
-create or replace procedure datashare.sp_grant_share_permissions()
-returns table (sql string)
+CREATE OR REPLACE PROCEDURE datashare.sp_grant_share_permissions()
+RETURNS TABLE (SQL STRING)
 LANGUAGE SQL
-execute as caller
-as
+EXECUTE AS CALLER
+AS
 $$
 DECLARE
-  results resultset;
+  results RESULTSET;
 BEGIN
-  results := (call datashare.sp_grant_share_permissions(:db, '2000-01-01'::TIMESTAMP_NTZ) );
-  RETURN table(results);
+  results := (CALL datashare.sp_grant_share_permissions(:db, '2000-01-01'::TIMESTAMP_NTZ) );
+  RETURN TABLE(results);
 END;
 $$
 ;
 {% endmacro %}
 
 {% macro create_sp_grant_share_permissions_string() %}
-create or replace procedure datashare.sp_grant_share_permissions(db string)
-returns table (sql string)
+CREATE OR REPLACE PROCEDURE datashare.sp_grant_share_permissions(db STRING)
+RETURNS TABLE (SQL STRING)
 LANGUAGE SQL
-execute as caller
-as
+EXECUTE AS CALLER
+AS
 $$
 DECLARE
-  results resultset;
+  results RESULTSET;
 BEGIN
-  results := (call datashare.sp_grant_share_permissions(:db, '2000-01-01'::TIMESTAMP_NTZ) );
-  RETURN table(results);
+  results := (CALL datashare.sp_grant_share_permissions(:db, '2000-01-01'::TIMESTAMP_NTZ) );
+  RETURN TABLE(results);
 END;
 $$
 ;
