c4267ad40c9e04e9f708899a833ea1f9e75c54e0
Julius Remigio <14811322+juls858@users.noreply.github.com>
2023-07-10 14:23:56 -0700

    - remove contradictary filter - do not run sql in dev

--- macros/utils.sql
+++ macros/utils.sql
@@ -2,7 +2,7 @@
     {% set sql %}
         call datashare.sp_grant_share_permissions({{ args }})
     {% endset %}
-    {% if execute %}
+    {% if execute and not target.database.upper().endswith("_DEV") %}
         {% set results = run_query(sql) %}
         {% do log(sql, True) %}
         {% do results.print_table(max_column_width=255) %}
