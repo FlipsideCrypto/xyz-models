e19415454cf527a79b608333334054beb4fb72f2
forgash_ <jmfxyz@pm.me>
2023-07-17 15:17:36 -0600

    core tests and upd gha script

--- models/core/core__fact_outputs.sql
+++ models/core/core__fact_outputs.sql
@@ -1,11 +1,13 @@
 {{ config(
-    materialized = 'view'
+    materialized = 'view',
+    tags = ['core']
 ) }}
 
 WITH outputs AS (
 
     SELECT
         * exclude (
+            output_data,
             _partition_by_block_id,
             _inserted_timestamp
         )
