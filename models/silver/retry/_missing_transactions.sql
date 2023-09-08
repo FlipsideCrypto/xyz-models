735df22f8d4b6f895ba9b8b1bc8affcf90991250
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2023-09-08 13:05:28 -0600

    upd _missing_transactions to incl null

--- models/silver/retry/_missing_transactions.sql
+++ models/silver/retry/_missing_transactions.sql
@@ -1,5 +1,5 @@
 {{ config(
-    materialized = 'ephemeral'
+    materialized = 'view'
 ) }}
 
 WITH blocks AS (
@@ -42,3 +42,4 @@ FROM
     LEFT JOIN transactions t USING (block_number)
 WHERE
     b.tx_count != t.tx_count
+    OR t.tx_count IS NULL
