760e10433c1fe29376ea5308eae4f89767c8a85e
Eric Laurello <eric.laurello@flipsidecrypto.com>
2023-10-30 16:21:35 -0400

    silver merge excludes

--- models/silver/core/silver__events.sql
+++ models/silver/core/silver__events.sql
@@ -2,6 +2,7 @@
     materialized = 'incremental',
     unique_key = ['tx_hash','event_index'],
     incremental_strategy = 'merge',
+    merge_exclude_columns = ["inserted_timestamp"],
     cluster_by = ['block_timestamp::DATE','_inserted_timestamp::DATE'],
     post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION ON EQUALITY(tx_hash, event_type,event_address,event_module,event_resource);"
 ) }}
