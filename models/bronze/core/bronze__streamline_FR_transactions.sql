8edd64b21111159b9c6f5350d4367aaeeaa520cf
Eric Laurello <eric.laurello@flipsidecrypto.com>
2023-11-15 14:57:41 -0500

    updates

--- models/bronze/core/bronze__streamline_FR_transactions.sql
+++ models/bronze/core/bronze__streamline_FR_transactions.sql
@@ -5,8 +5,8 @@
 
 {% set model = this.identifier.split("_") [-1] %}
 {{ streamline_external_table_FR_query(
-    model = "blocks_tx",
+    model = "transactions",
     partition_function = "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 3), '_', 1) AS INTEGER )",
-    partition_name = "_partition_by_block_id",
+    partition_name = "_partition_by_tx_version",
     unique_key = "partition_key"
 ) }}
