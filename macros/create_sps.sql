28997bdeca848eb0369194040e4a930e3f4293ed
Ryan-Loofy <63126328+Ryan-Loofy@users.noreply.github.com>
2023-11-16 11:20:54 -0500

    Formatting updates

--- macros/create_sps.sql
+++ macros/create_sps.sql
@@ -5,11 +5,13 @@
             {{ sp_create_prod_clone('_internal') }};
 
         {% endif %}
+        
         CREATE SCHEMA IF NOT EXISTS datashare;
         {{ create_sp_grant_share_permissions_string_timestamp() }}
         {{ create_sp_grant_share_permissions_timestamp() }}
         {{ create_sp_grant_share_permissions() }}
         {{ create_sp_grant_share_permissions_string() }}
+
         CREATE SCHEMA IF NOT EXISTS streamline;
         {{create_sp_create_streamline_users_roles() }}
         {{create_sp_streamline_dev_permissions() }}
