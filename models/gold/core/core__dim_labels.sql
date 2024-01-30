3a1cadb64e95618c951c712085499dbd83ecb45b
WHYTEWYLL <49686519+WHYTEWYLL@users.noreply.github.com>
2024-01-30 15:21:19 +0100

    deleted colapse

--- models/gold/core/core__dim_labels.sql
+++ models/gold/core/core__dim_labels.sql
@@ -13,6 +13,6 @@ SELECT
     project_name AS label,
     labels_combined_id AS dim_labels_id,
     inserted_timestamp as inserted_timestamp,
-    COALESCE(modified_timestamp, '2000-01-01' :: TIMESTAMP_NTZ) as modified_timestamp
+    modified_timestamp as modified_timestamp
 FROM
     {{ ref('silver__labels') }}
