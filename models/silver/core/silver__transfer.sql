7899cb3e132de7844aef269c54ca32e3458771ad
WHYTEWYLL <49686519+WHYTEWYLL@users.noreply.github.com>
2024-02-13 18:31:20 +0100

    silver transfer base

--- models/silver/core/silver__transfer.sql
+++ models/silver/core/silver__transfer.sql
@@ -1,75 +1,113 @@
 {{ config(
-    materialized = 'incremental',
-    incremental_strategy = 'merge',
-    merge_exclude_columns = ["inserted_timestamp"],
-    unique_key = 'tx_id',
-    cluster_by = ["tx_id"],
+  materialized = 'incremental',
+  incremental_strategy = 'merge',
+  merge_exclude_columns = ["inserted_timestamp"],
+  unique_key = 'tx_id',
+  cluster_by = ["from_entity", "to_entity"],
 ) }}
 
 WITH inputs AS (
+
+  SELECT
+    *
+  FROM
+    {{ ref('silver__inputs_final') }}
+
+{% if is_incremental() %}
+WHERE
+  _inserted_timestamp >= (
     SELECT
-        *
+      MAX(_inserted_timestamp) _inserted_timestamp
     FROM
-        {{ ref('silver__inputs_final') }} -- -- cluster_by "block_number", "tx_id"
-    WHERE
-        block_number >= 813567
-        AND block_number <= 813692
-    -- {% if is_incremental() %}
-    --     _inserted_timestamp >= (
-    --         SELECT
-    --             MAX(_inserted_timestamp) _inserted_timestamp
-    --         FROM
-    --             {{ this }}
-    -- )
-    -- {% endif %}
-), 
+      {{ this }}
+  )
+{% endif %}
+),
 output AS (
+  SELECT
+    *
+  FROM
+    {{ ref('silver__outputs_final') }}
+
+{% if is_incremental() %}
+WHERE
+  _inserted_timestamp >= (
     SELECT
-        *
+      MAX(_inserted_timestamp) _inserted_timestamp
     FROM
-        {{ ref('silver__inputs_final') }} -- cluster_by ["_partition_by_block_id", "tx_id"],
-    WHERE
-        block_number >= 813567
-    AND block_number <= 813692
+      {{ this }}
+  )
+{% endif %}
 ),
 full_entity_cluster AS (
-    SELECT
-        *
-    FROM
-        {{ ref('silver__full_entity_cluster') }}
+  SELECT
+    *
+  FROM
+    {{ ref('silver__full_entity_cluster') }}
 ),
-
 inputs_mapped AS (
   SELECT
     tx_id,
-    COALESCE(project_name, CAST(address_group AS varchar)) AS from_entity
+    COALESCE(
+      address_group :VARCHAR,
+      pubkey_script_address
+    ) AS from_entity,
+    IFF(
+      address_group IS NOT NULL,
+      FLOOR(
+        address_group,
+        -3
+      ),
+      0
+    ) AS _partition_by_address_group_from_entity
   FROM
-        inputs fi
-    LEFT JOIN full_entity_cluster ec ON fi.pubkey_script_address = ec.address
-  group by
+    inputs fi
+    LEFT JOIN full_entity_cluster ec
+    ON fi.pubkey_script_address = ec.address
+  GROUP BY
     tx_id,
-    from_entity
-) 
+    from_entity,
+    _partition_by_address_group
+),
+FINAL AS (
+  SELECT
+    fo.tx_id,
+    block_timestamp,
+    from_entity,
+    COALESCE(
+      address_group :VARCHAR,
+      pubkey_script_address
+    ) AS to_entity,
+    IFF(
+      address_group IS NOT NULL,
+      FLOOR(
+        address_group,
+        -3
+      ),
+      0
+    ) AS _partition_by_address_group_to_entity,
+    _partition_by_address_group_from_entity,
+    SUM(VALUE) AS transfer_amount
+  FROM
+    output fo
+    LEFT JOIN full_entity_cluster ec
+    ON fo.pubkey_script_address = ec.address
+    LEFT JOIN inputs_mapped
+    ON inputs_mapped.tx_id = fo.tx_id
+  GROUP BY
+    fo.tx_id,
+    block_timestamp,
+    from_entity,
+    to_entity,
+    _partition_by_address_group_to_entity,
+    _partition_by_address_group_from_entity
+)
 SELECT
-  fo.tx_id,
-  block_timestamp,
-  from_entity,
-  COALESCE(
-    project_name,
-    CAST(address_group AS varchar),
-    PUBKEY_SCRIPT_ADDRESS
-  ) AS to_entity,
-  sum(value) AS transfer_amount,
+  *,
+  {{ dbt_utils.generate_surrogate_key(
+    ['tx_id', 'from_entity', 'to_entity']
+  ) }} AS transfer_id,
   SYSDATE() AS inserted_timestamp,
-  SYSDATE()   AS modified_timestamp
+  SYSDATE() AS modified_timestamp
 FROM
-  output fo 
-  LEFT JOIN full_entity_cluster ec ON fo.pubkey_script_address = ec.address
-  LEFT JOIN inputs_mapped ON inputs_mapped.tx_id = fo.tx_id
-GROUP BY
-  fo.tx_id,
-  block_timestamp,
-  from_entity,
-  to_entity
-
-  -- 1 day is 17s
+  FINAL
