d0b4025e0808eab79d5a89bace2a91b5b4ca3d81
forgash_ <jmfxyz@pm.me>
2024-02-14 15:00:52 -0700

    upd core

--- models/gold/core/core__dim_labels.sql
+++ models/gold/core/core__dim_labels.sql
@@ -7,11 +7,12 @@ SELECT
     blockchain,
     creator,
     address,
-    address_name,
     label_type,
     label_subtype,
+    address_name,
+    project_name,
     project_name AS label,
-    labels_combined_id AS dim_labels_id,
+    labels_id AS dim_labels_id,
     inserted_timestamp as inserted_timestamp,
     modified_timestamp as modified_timestamp
 FROM
