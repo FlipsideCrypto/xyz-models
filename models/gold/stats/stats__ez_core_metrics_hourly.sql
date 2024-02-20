45ca392fd9745b583bc7cc4cd09d5d08df993957
tarikceric <tarik@flipsidecrypto.com>
2024-02-20 14:45:51 -0800

    add aptos stats

---
+++ models/gold/stats/stats__ez_core_metrics_hourly.sql
@@ -0,0 +1,37 @@
+{{ config(
+    materialized = 'view',
+    persist_docs ={ "relation": true,
+    "columns": true },
+    meta ={ 'database_tags':{ 'table':{ 'PURPOSE': 'STATS, METRICS, CORE, HOURLY',
+    } } },
+    tags = ['noncore']
+) }}
+
+SELECT
+    block_timestamp_hour,
+    block_number_min,
+    block_number_max,
+    block_count,
+    transaction_count,
+    transaction_count_success,
+    transaction_count_failed,
+    unique_sender_count,
+    unique_payload_function_count,
+    total_fees AS total_fees_native,
+    ROUND(
+        (total_fees / pow(
+            10,
+            8
+        )) * p.price,
+        2
+    ) AS total_fees_usd,
+    core_metrics_hourly_id AS ez_core_metrics_hourly_id,
+    s.inserted_timestamp AS inserted_timestamp,
+    s.modified_timestamp AS modified_timestamp
+FROM
+    {{ ref('silver__core_metrics_hourly') }}
+    s
+    LEFT JOIN {{ ref('price__ez_hourly_token_prices') }}
+    p
+    ON s.block_timestamp_hour = p.hour
+    AND p.token_address = '0x1::aptos_coin::aptoscoin'
