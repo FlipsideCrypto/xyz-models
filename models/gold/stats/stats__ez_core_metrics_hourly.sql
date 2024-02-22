191fd6b368a04c651194dfff05cbf6fc787e056b
WHYTEWYLL <49686519+WHYTEWYLL@users.noreply.github.com>
2024-02-22 16:12:24 +0100

    bitcoin stats table

---
+++ models/gold/stats/stats__ez_core_metrics_hourly.sql
@@ -0,0 +1,28 @@
+{{ config(
+    materialized = 'view',
+    tags = ['core']
+) }}
+
+SELECT
+    block_timestamp_hour,
+    block_number_min,
+    block_number_max,
+    block_count,
+    transaction_count,
+    unique_from_count as unique_address_count,
+    unique_to_count,
+    total_fees AS total_fees_native,
+    ROUND(
+        total_fees * p.close,
+        2
+    ) AS total_fees_usd,
+    core_metrics_hourly_id AS ez_core_metrics_hourly_id,
+    s.inserted_timestamp AS inserted_timestamp,
+    s.modified_timestamp AS modified_timestamp
+FROM
+    {{ ref('silver_stats__core_metrics_hourly') }}
+    s
+    LEFT JOIN {{ ref('price__fact_hourly_token_prices') }}
+    p
+    ON s.block_timestamp_hour = p.hour
+    AND p.provider = 'coinpaprika'
