df1f5ee350e035fa274fbed744499ecaddac4b25
shah <info@shahnewazkhan.ca>
2024-04-09 12:17:13 -0700

    add bronze / silver blocks artifacts

---
+++ models/streamline/quantum/poc/core/streamline__aptos_blocks.sql
@@ -0,0 +1,21 @@
+{{ config (
+    materialized = "view",
+    tags = ['quantum_poc','streamline_view']
+) }}
+
+{% if execute %}
+    {% set height = run_query("SELECT live.udf_api( 'GET', 'https://fullnode.mainnet.aptoslabs.com/v1', OBJECT_CONSTRUCT( 'Content-Type', 'application/json' ),{} ):data:block_height::INT") %}
+    {% set block_height = height.columns [0].values() [0] %}
+{% else %}
+    {% set block_height = 0 %}
+{% endif %}
+
+SELECT
+    _id AS block_number
+FROM
+    {{ source(
+        'crosschain_silver',
+        'number_sequence'
+    ) }}
+WHERE
+    _id <= {{ block_height }}
---
+++ models/streamline/quantum/poc/silver/silver__blocks.sql
@@ -0,0 +1,50 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = "block_number",
+    incremental_strategy = 'merge',
+    merge_exclude_columns = ["inserted_timestamp"],
+    cluster_by = ['block_timestamp::DATE','_inserted_timestamp::DATE'],
+    tags = ['core','full_test']
+) }}
+-- depends_on: {{ ref('bronze__streamline_blocks_tx') }}
+
+SELECT
+    partition_key AS block_number,
+    DATA :block_hash :: STRING AS block_hash,
+    DATA :block_timestamp :: bigint AS block_timestamp_num,
+    TO_TIMESTAMP(
+        block_timestamp_num :: STRING
+    ) AS block_timestamp,
+    DATA :first_version :: bigint AS first_version,
+    DATA :last_version :: bigint AS last_version,
+    ARRAY_SIZE(
+        DATA :transactions
+    ) AS tx_count_from_transactions_array,
+    last_version - first_version + 1 AS tx_count_from_versions,
+    {{ dbt_utils.generate_surrogate_key(
+        ['block_number']
+    ) }} AS blocks_id,
+    SYSDATE() AS inserted_timestamp,
+    SYSDATE() AS modified_timestamp,
+    _inserted_timestamp,
+    '{{ invocation_id }}' AS _invocation_id
+FROM
+
+{% if is_incremental() %}
+{{ ref('bronze__streamline_blocks_tx') }}
+WHERE
+    _inserted_timestamp >= (
+        SELECT
+            MAX(
+                _inserted_timestamp
+            )
+        FROM
+            {{ this }}
+    )
+{% else %}
+    {{ ref('bronze__streamline_FR_blocks_tx') }}
+{% endif %}
+
+qualify(ROW_NUMBER() over(PARTITION BY block_number
+ORDER BY
+    _inserted_timestamp DESC)) = 1
