f8cc65fb8e00fd23cd8212791bf9f20d4b60afeb
forgash_ <jmfxyz@pm.me>
2024-04-23 15:36:48 -0600

    block inscription count model

---
+++ models/silver/nft/silver__block_inscription_count.sql
@@ -0,0 +1,76 @@
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy = 'merge',
+    merge_exclude_columns = ["inserted_timestamp"],
+    unique_key = 'block_hash',
+    cluster_by = ["block_number"],
+    tags = ["ordhook"]
+) }}
+
+WITH blocks AS (
+
+    SELECT
+        block_number,
+        block_hash,
+        modified_timestamp AS _modified_timestamp
+    FROM
+        {{ ref('silver__blocks') }}
+    WHERE
+        block_number >= 767430
+        AND NOT is_pending
+
+{% if is_incremental() %}
+AND (
+    _modified_timestamp >= (
+        SELECT
+            MAX(_modified_timestamp)
+        FROM
+            {{ this }}
+    )
+    OR block_number > (
+        SELECT
+            MAX(block_number)
+        FROM
+            {{ this }}
+        WHERE
+            status_code = 200
+    )
+)
+{% endif %}
+ORDER BY
+    block_number ASC
+LIMIT
+    100
+),
+get_inscription_count AS (
+    SELECT
+        block_number,
+        block_hash,
+        _modified_timestamp,
+        SYSDATE() AS _request_timestamp,
+        {{ target.database }}.live.udf_api(
+            'GET',
+            'https://api.hiro.so/ordinals/v1/inscriptions/transfers?block=' || block_hash || '&limit=1',
+            {},
+            OBJECT_CONSTRUCT(
+                'Accept',
+                'application/json',
+                'x-hiro-api-key',
+                'db2c8f0f3262113265682c15636bc775' -- TODO move to vault
+            )
+        ) AS response
+    FROM
+        blocks
+)
+SELECT
+    block_number,
+    block_hash,
+    response :data :total :: NUMBER AS inscription_count,
+    response :status_code :: NUMBER AS status_code,
+    _request_timestamp,
+    _modified_timestamp,
+    SYSDATE() AS inserted_timestamp,
+    SYSDATE() AS modified_timestamp,
+    '{{ invocation_id }}' AS _invocation_id
+FROM
+    get_inscription_count
