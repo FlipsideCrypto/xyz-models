5942886c26c238ac126b2ae5ad6f1ae32bf1c168
Eric Laurello <eric.laurello@flipsidecrypto.com>
2024-04-25 11:13:41 -0400

    GHA and final other updates

--- macros/utils.sql
+++ macros/utils.sql
@@ -8,3 +8,14 @@
         {% do results.print_table(max_column_width=255) %}
     {% endif %}
 {% endmacro %}
+
+{% macro call_sp_grant_share_permissions_v2() %}
+    {% set sql %}
+        call {{ target.database }}.datashare.sp_share_build_and_grant_permissions()
+    {% endset %}
+    {% if execute and not target.database.upper().endswith("_DEV") %}
+        {% set results = run_query(sql) %}
+        {% do log(sql, True) %}
+        {% do results.print_table(max_column_width=255) %}
+    {% endif %}
+{% endmacro %}
