48aedf4658603732baba5004c7edb8c9a14f6645
Eric Laurello <eric.laurello@flipsidecrypto.com>
2024-06-11 09:01:02 -0400

    dial down blocks

--- models/streamline/silver/realtime/streamline__blocks_tx_realtime.sql
+++ models/streamline/silver/realtime/streamline__blocks_tx_realtime.sql
@@ -4,9 +4,9 @@
         func = 'streamline.udf_bulk_rest_api_v2',
         target = "{{this.schema}}.{{this.identifier}}",
         params ={ "external_table" :"blocks_tx",
-        "sql_limit" :"500000",
-        "producer_batch_size" :"10000",
-        "worker_batch_size" :"5000",
+        "sql_limit" :"100000",
+        "producer_batch_size" :"2000",
+        "worker_batch_size" :"1000",
         "sql_source" :"{{this.identifier}}" }
     )
 ) }}
